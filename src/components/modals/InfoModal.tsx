13f4a99fea3080471cb41a997f3be9c7badbd7df
Citrik <minebloxroyale781@gmail.com>
2022-03-02 17:23:45 -0500

    Initial commit

---
+++ src/components/modals/InfoModal.tsx
@@ -0,0 +1,71 @@
+import { Cell } from '../grid/Cell'
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const InfoModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="How to play Techdle" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Guess the word in 6 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the word.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="C"
+          status="correct"
+        />
+        <Cell value="O" />
+        <Cell value="R" />
+        <Cell value="D" />
+        <Cell value="s" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter C is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="E" />
+        <Cell value="M" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="A"
+          status="present"
+        />
+        <Cell value="I" />
+        <Cell value="L" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter A is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="M" />
+        <Cell value="O" />
+        <Cell value="D" />
+        <Cell isRevealing={true} isCompleted={true} value="E" status="absent" />
+        <Cell value="M" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter E is not in the word in any spot.
+      </p>
+
+      <p className="justify-center mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        This is a spinoff of the popular New York Times game, ㅤㅤㅤWordle. Test your technology vocab skills!{' ㅤㅤㅤ'}
+        <a
+          href="https://www.buymeacoffee.com/Citrik"
+          className="underline font-bold"
+        >
+          Buy me a coffee!
+        </a>{' '}
+      </p>
+    </BaseModal>
+  )
+}
