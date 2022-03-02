1cb55e217cdfa292fa00566f0941c063db365e3e
Citrik <64604942+GangstaGaming@users.noreply.github.com>
2022-03-02 18:07:56 -0500

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -60,6 +60,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       <p className="flex justify-center mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is a spinoff of the popular New York Times game, Wordle. Test your technology vocab skills!{' ㅤㅤㅤ'}
       <br>
+      </br>
         <a
           href="https://www.buymeacoffee.com/Citrik"
           className="underline font-bold"
